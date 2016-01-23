ffb75d9775ee5eaae0cd164979476d04720f2ad2
Rodrigo Navarro <rnavarro1@gmail.com>
2016-01-23 10:03:46 -0200

    Detecting all the references of a given formula.

--- lib/spreadshit/formula.rb
+++ lib/spreadshit/formula.rb
@@ -2,20 +2,56 @@ require "spreadshit/formula/parser"
 
 class Spreadshit
   module Formula
-    class Literal < Struct.new(:content); end
+    module Travarsable
+      include Enumerable
+
+      def children
+        []
+      end
+
+      def each(&block)
+        block.call self
+        children.each { |child| child.each(&block) }
+      end
+
+      def references
+        find_all { |expression| expression.is_a? Reference }.to_set
+      end
+    end
+
+    class Literal < Struct.new(:content)
+      include Travarsable
+    end
+
     class String < Literal; end
     class Number < Literal; end
     class Integer < Number; end
     class Decimal < Number; end
 
-    class BinaryOperation < Struct.new(:left, :right); end
+    class BinaryOperation < Struct.new(:left, :right)
+      include Travarsable
+
+      def children
+        [left, right]
+      end
+    end
+
     class Addition < BinaryOperation; end
     class Subtraction < BinaryOperation; end
     class Multiplication < BinaryOperation; end
     class Division < BinaryOperation; end
-    class Function < Struct.new(:name, :arguments); end
+
+    class Function < Struct.new(:name, :arguments)
+      include Travarsable
+
+      def children
+        arguments
+      end
+    end
 
     class Reference < Struct.new(:col, :row)
+      include Travarsable
+
       def address
         [col, row].join
       end
@@ -26,6 +62,8 @@ class Spreadshit
     end
 
     class Range < Struct.new(:top, :bottom)
+      include Travarsable
+
       def to_s
         [top.address, bottom.address].join(":")
       end
@@ -38,6 +76,10 @@ class Spreadshit
         end
 
         Matrix[*refs].transpose
+      end
+
+      def children
+        to_matrix.to_a.flatten
       end
     end
   end
